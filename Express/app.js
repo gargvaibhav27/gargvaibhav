058479e66305e4e2e7a25677bc2c9d00286ea5d1
gargvaibhav27 <gargvaibhav2711@gmail.com>
2026-02-24 11:50:53 +0530

    express

---
+++ Express/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('this is my first server');
+})
+
+app.get('/about', (req, res) => {
+    res.send('this is about page');
+})
+
+app.listen(port, () => {  
+    console.log(`server is run at:http://localhost:${port}`);
+})
