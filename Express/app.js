d936b057f583727b40bc3f821e55adbd28375f5f
gargvaibhav27 <gargvaibhav2711@gmail.com>
2026-03-20 14:45:31 +0530

    h

--- Express/app.js
+++ Express/app.js
@@ -1,17 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-    res.send('this is my first server');
-})
+//Data Base(json) json is used to store data and transfer the data. object store karne ke liye use hota hai.
+const students = [
+  {
+    id: 1,
+    name: "hani",
+  },
+  {
+    id: 2,
+    name: "hani1",
+  },
+];
 
-app.get('/about', (req, res) => {
-    res.send('this is about page');
-})
-app.listen(port, () => {  
-    console.log(`server is run at:http://localhost:${port}`);
-})
+//API for read all data(R)
+
+app.get("/read", (req, res) => {
+  response.json(students);
+});
+
+app.listen(port, () => {
+  console.log(`server is run at:http://localhost:${port}`);
+});
+// app.get('/', (req, res) => {
+//     res.send('this is my first server');
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('this is about page');
+// })
 // node app.js for running on terminal
 // npm init -y for backend
 // npm create vite@latest for frontend
